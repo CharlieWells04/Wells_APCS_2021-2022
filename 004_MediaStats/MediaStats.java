eac07b5483c8f4860af2b53ded3b311a68a14061
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-09-14 14:30:53 -0500

    Create MediaStats.java

---
+++ 004_MediaStats/MediaStats.java
@@ -0,0 +1,16 @@
+public class MediaStats {
+    public static void main(String[] args){
+        int iceAgeLength = 81;
+        int iceAgeReleaseDate = 2002;
+        int iceAgeTwoLength = 91;
+        int iceAgeTwoReleaseDate = 2004;
+        int iceAgeThreeLength = 94;
+        int iceAgeThreeReleaseDate = 2009;
+
+        System.out.println("The movie Ice Age came out in " + iceAgeReleaseDate + " and was " + iceAgeLength + " minutes long" );
+        System.out.println("The movie Ice Age 2 came out in " + iceAgeTwoReleaseDate + " and was " + iceAgeTwoLength + " minutes long" );
+        System.out.println("The movie Ice Age 3 came out in " + iceAgeThreeReleaseDate + " and was " + iceAgeThreeLength + " minutes long" );
+    }
+    
+}
+// stay hydrated 
