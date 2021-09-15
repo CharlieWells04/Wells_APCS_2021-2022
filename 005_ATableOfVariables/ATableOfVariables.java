16b8385cd72470e98541cf0d32e81b9e35255fd8
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-09-15 12:10:55 -0500

    Create ATableOfVariables.java

---
+++ 005_ATableOfVariables/ATableOfVariables.java
@@ -0,0 +1,21 @@
+public class ATableOfVariables {
+    public static void main(String[] args){
+       int r1C1 = 1;
+       int r1C2 = 1;
+       int r2C1 = 4;
+       int r2C2 = 8;
+       int r3C1 = 9;
+       int r3C2 = 27;
+       int r4C1 = 16;
+       int r4C2 = 64;
+
+       System.out.println("a   a^2   a^3");
+       System.out.println("1   " + r1C1 + "    " + r1C2);
+       System.out.println("2   " + r2C1 + "    " + r2C2);
+       System.out.println("3   " + r3C1 + "    " + r3C2);
+       System.out.println("4   " + r4C1 + "   " + r4C2);
+    }   
+
+
+
+}
