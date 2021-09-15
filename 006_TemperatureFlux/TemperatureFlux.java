8e520268512e533279de6c22dc088293354f6a8d
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-09-15 12:20:38 -0500

    Create TemperatureFlux.java

---
+++ 006_TemperatureFlux/TemperatureFlux.java
@@ -0,0 +1,16 @@
+public class TemperatureFlux {
+    public static void main(String[] args){
+        int highTemp = 76;
+        int lowTemp = 63;
+        int difference = (highTemp - lowTemp);
+
+        System.out.println("The high temp fot today is " + highTemp + ", the low temp is " + lowTemp + ", and the diffrence is " + difference);
+        // 3 different variables
+        
+
+
+
+    }
+
+    
+}
