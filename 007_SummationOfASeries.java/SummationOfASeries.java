b3a660400cb0af469a57ed13a61c8008ba05ee14
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-09-15 12:30:28 -0500

    Create SummationOfASeries.java

---
+++ 007_SummationOfASeries.java/SummationOfASeries.java
@@ -0,0 +1,23 @@
+public class SummationOfASeries {
+    public static void main(String[] args){
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int num4 = 4;
+        int num5 = 5;
+        int num6 = 6;
+        int num7 = 7;
+        int num8 = 8;
+        int num9 = 9;
+        // the sum int
+        int summ = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9);
+
+        System.out.println(summ);
+
+
+
+
+    }
+
+}
+
