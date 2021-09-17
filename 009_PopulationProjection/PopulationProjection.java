ad2d65baf20f61c3429d31d870fff158771617b5
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-09-17 13:45:06 -0500

    Create PopulationProjection.java

---
+++ 009_PopulationProjection/PopulationProjection.java
@@ -0,0 +1,18 @@
+public class PopulationProjection {
+    public static void main(String[] args)
+    {
+        int secondsPerBirth = 7;
+        int secondsPerDeath = 13;
+        int secondsPerImmigrant = 45;
+        int currentPop = 312_032_486;
+        int secondsPerDay = 86400;
+        double birthsPerDay = (secondsPerDay / secondsPerDay);
+        double deathsPerDay = (secondsPerDay / secondsPerDeath);
+        double immigrantsPerDay = (secondsPerDay / secondsPerImmigrant);
+        int daysPerYear = 365
+        double birthsPerYear = (daysPerYear )
+
+
+    }
+    
+}
