99818ea044a64d3fba41efa3e64285da2c140b70
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-09-21 11:55:04 -0500

    Create BearCounter.java

---
+++ 012_BearCounter/BearCounter.java
@@ -0,0 +1,22 @@
+public class BearCounter {
+    public static void main(String[] args){
+        int bearsInClearing = 0;
+        System.out.println("There are " + bearsInClearing + " bears in the clearing");
+        bearsInClearing++;
+        System.out.println("A bear enters, There is " + bearsInClearing + " bear in the clearing");
+        bearsInClearing++;
+        System.out.println("A bear enters, There are " + bearsInClearing + " bears in the clearing");
+        bearsInClearing ++;
+        System.out.println("A bear enters, There are " + bearsInClearing + " bears in the clearing");
+        bearsInClearing--;
+        System.out.println("A bear leaves, There are " + bearsInClearing + " bears in the clearing");
+        bearsInClearing--;
+        System.out.println("A bear leaves, There is " + bearsInClearing + " bear in the clearing");
+        bearsInClearing--;
+        System.out.println("A bear leaves, There are " + bearsInClearing + " bears in the clearing");
+        
+
+
+    }
+    
+}
