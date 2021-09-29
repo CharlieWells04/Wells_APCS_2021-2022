8b37ef6a73c166fc5b03b5cdcc1e6e488e6a19d3
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-09-29 11:38:21 -0500

    Update Garage.java

---
+++ Lecture Follow/Unit_2_Garage/Garage.java
@@ -0,0 +1,13 @@
+public class Garage {
+    public static void main(String[] args){
+        Car myCar = new Car(1423, -3000, "Lamborghini", "Thingy");
+
+        System.out.println(myCar.year);
+        System.out.println(myCar.manufacturerName);
+        System.out.println(myCar.modelName);
+        System.out.println(myCar.milesDriven);         
+    }
+    
+}
+
+ 
