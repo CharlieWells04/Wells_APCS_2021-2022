3e2c1a05c5bb403c660af8bea6fa2a19fe8c3e4c
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-10-08 11:03:15 -0500

    Create CarBuilderOne.java

---
+++ 021_CarBuilderOne/CarBuilderOne.java
@@ -0,0 +1,12 @@
+//import scanner
+import java.util.Scanner;
+
+public class CarBuilderOne {
+    public static void main(String[] args);
+    {
+        //declare scanner
+        Scanner scan = new Scanner(System.in);
+        
+    }
+    
+}
