081391a59a638555633b693e3d907e6cd9af96d6
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-10-08 10:23:52 -0500

    Create LoanCalculator.java

---
+++ 020_LoanCalculator/LoanCalculator.java
@@ -0,0 +1,26 @@
+//import scanner
+import java.util.Scanner;
+
+public class LoanCalculator {
+    public static void main(String[] args){
+        // declare scanner
+        Scanner scan = new Scanner(System.in);
+        // ask and store priciple balance
+        System.out.println("What is your principle balance?");
+        String principleBalance = scan.nextLine();
+        // ask and store annual interest rate
+        System.out.println("What is your anual interest rate?");
+        String annualInterestRate = scan.nextLine();
+        //ask and store remaining months
+        System.out.println("How many months do you have left");
+        String remainingMonths = scan.nextLine();
+
+        //calculations (need to cast)
+
+
+
+
+
+    }
+    
+}
