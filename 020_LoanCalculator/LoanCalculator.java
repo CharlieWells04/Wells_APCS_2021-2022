8f6d852c5e7ec2c258c990ba2402e17a3b1046bf
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-10-08 11:03:19 -0500

    Update LoanCalculator.java

--- 020_LoanCalculator/LoanCalculator.java
+++ 020_LoanCalculator/LoanCalculator.java
@@ -6,21 +6,24 @@ public class LoanCalculator {
         // declare scanner
         Scanner scan = new Scanner(System.in);
         // ask and store priciple balance
-        System.out.println("What is your principle balance?");
-        String principleBalance = scan.nextLine();
+        System.out.println("What is your principle balance? ");
+        double principleBalance = scan.nextDouble();
         // ask and store annual interest rate
-        System.out.println("What is your anual interest rate?");
-        String annualInterestRate = scan.nextLine();
+        System.out.println("What is your anual interest rate? (enter as decimal)");
+        double annualInterestRate = scan.nextDouble();
         //ask and store remaining months
-        System.out.println("How many months do you have left");
-        String remainingMonths = scan.nextLine();
-
-        //calculations (need to cast)
-
+        System.out.println("How many months do you have left ");
+        double remainingMonths = scan.nextDouble();
+        scan.close();
 
 
+        //calculations 
+        double monthlyInterest = principleBalance * (annualInterestRate/remainingMonths);
+        double monthlyPayment = principleBalance/remainingMonths + monthlyInterest;
 
-
+        // print statements
+        System.out.println("Your monthly interest is " + monthlyInterest);
+        System.out.println("Your monthly payment is " + monthlyPayment);
     }
     
 }
