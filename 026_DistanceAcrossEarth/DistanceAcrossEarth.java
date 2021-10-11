d3a57d21e646ad5c9e91ed1f552dd541a9b9525e
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-10-11 14:43:25 -0500

    Create DistanceAcrossEarth.java

---
+++ 026_DistanceAcrossEarth/DistanceAcrossEarth.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DistanceAcrossEarth {
+    public static void main(String[] args){
+        
+        //decalare scanner, ask questions, store variables
+        Scanner scan = new Scanner(System.in);
+        S
+
+    }
+
+
+
+
+    
+}
