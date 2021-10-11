6b5a1a757b4453582b3555bd1ad5f40fc5fc197b
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-10-11 12:22:09 -0500

    Update MaximumIntegers.java

--- 022_MaximumIntegers/MaximumIntegers.java
+++ 022_MaximumIntegers/MaximumIntegers.java
@@ -1,7 +1,22 @@
 public class MaximumIntegers {
     public static void main(String[] args)
     {
+        // min and max values
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        //print statements
+        System.out.println(max);
+        System.out.println(min);
+
+        //overflow values
+        int maxOverFlow = max + (max + 1000002);
+        int minOverFlow = min + (min + 745);
         
+        //print statements
+        System.out.println(maxOverFlow);
+        System.out.println(minOverFlow);
+
     }
     
 }
