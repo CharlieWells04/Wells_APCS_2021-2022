585ce8b3b088b4e738fd70cc8fd06c6e65b3def9
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-10-12 12:32:27 -0500

    Create PerimeterAreaOfAHexagonCalculator.java

---
+++ 025_PerimeterAreaOfAHexigonCalculator/PerimeterAreaOfAHexagonCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PerimeterAreaofAHexagonCalculator {
+    public static void main(String[] args)
+    {
+        //declare scanner, ask question, store answer
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the length of the side of the hexagon?");
+        double sideLength = scan.nextDouble();
+        scan.close();
+
+        // using the formulas
+        double perimeter = (6 * sideLength);
+        double area = (((3 * Math.sqrt(3)) / 2) * Math.pow(sideLength, 2));
+
+        //print statements
+        System.out.println("The perimeter is " + perimeter);
+        System.out.println("The area is " + area); 
+    }
+}
