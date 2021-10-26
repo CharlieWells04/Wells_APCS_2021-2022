3ae485313a381860cfb406a1c4265ba7c06aa8c8
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-10-26 13:26:40 -0500

    Create EvenOrOdd.java

---
+++ 030_EvenOrOdd/EvenOrOdd.java
@@ -0,0 +1,25 @@
+//import scanner
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args)
+    {
+        //declare scanner
+        Scanner scan = new Scanner(System.in);
+        //ask question nd store variable
+        System.out.println("Enter you integer");
+        int theNumber = scan.nextInt();
+        scan.close();
+        //use remaider when divided by 2
+        int theRemainder = theNumber % 2;
+        //if satement, use remainder, if 0 is even
+        if (theRemainder == 0)
+        {
+            System.out.println("Your number is even");
+        }
+        else
+        {
+            System.out.println("Your number is odd");
+        }
+    }   
+}
