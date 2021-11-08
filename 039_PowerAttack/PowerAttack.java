ae9e97e164b7823a6aabb3ffa7f6947a6c6e902d
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-08 14:45:08 -0600

    Create PowerAttack.java

---
+++ 039_PowerAttack/PowerAttack.java
@@ -0,0 +1,11 @@
+public class PowerAttack {
+    public static void main(String[] args)
+    {
+       //random damage number using Math.random
+       int randoDamage = (int)(Math.random() * 10 + 1);
+       //declare other nessicary number
+       int baseStrength = 3;
+       int monsterHealth = 13
+        while(monsterHealth > 0)//while loop,repeates until monster is dead
+    }   
+}
