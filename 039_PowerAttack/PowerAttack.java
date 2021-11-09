41a12289d8f5f0cb2023c0ac1510006ccf08fc59
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-08 20:19:06 -0600

    Update PowerAttack.java

--- 039_PowerAttack/PowerAttack.java
+++ 039_PowerAttack/PowerAttack.java
@@ -1,11 +1,27 @@
 public class PowerAttack {
     public static void main(String[] args)
     {
-       //random damage number using Math.random
-       int randoDamage = (int)(Math.random() * 10 + 1);
-       //declare other nessicary number
-       int baseStrength = 3;
-       int monsterHealth = 13
+        //declare nessicary numbers
+        int randoStrength = 0;
+        int baseStrength = 3;
+        int monsterHealth = 13;
+        int finalStrength = 0;
+        int damage = 0;
         while(monsterHealth > 0)//while loop,repeates until monster is dead
+        {
+            randoStrength = (int)(Math.random() * 10 + 1);
+            finalStrength = randoStrength + baseStrength;
+            if(finalStrength >= 11)
+            {
+                damage = (int)(Math.random() * 3 + 4);
+                monsterHealth = monsterHealth - damage;
+                System.out.println("The attack was successful, you did " + damage + " damage, the monster has " + monsterHealth + " health left");
+            }
+            else
+            {
+                System.out.println("The attack failed, the monster has " + monsterHealth + " health left");
+            }
+        }
+        System.out.println("The monster has been slain!, good luck on further ventures");
     }   
 }
