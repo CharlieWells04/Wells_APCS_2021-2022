01f57961eb63220706bfacc462c1cc30780a7f80
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-15 12:11:20 -0600

    Create SimpleCounterButAFoorLoop.java

---
+++ 040_SimpleCounterButAForLoop/SimpleCounterButAFoorLoop.java
@@ -0,0 +1,25 @@
+public class SimpleCounterButAFoorLoop {
+    public static void main(String[] args){
+        //loop for 0 to 300
+        for(int i = 0; i <= 300; i++)
+        {
+            System.out.println(i);
+        }
+        //loop for 300 to 0
+        for(int i = 300; i >= 0; i--)
+        {
+            System.out.println(i);
+        }
+        //loop for 4798 to 5234
+        for (int i = 4798; i <= 5324; i++)
+        {
+            System.out.println(i);
+        }
+        //loop for 5324 to 4798
+        for (int i = 5324; i >= 4798; i--)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
