282d702afbaaee6e67421aab4eca06971849d9f5
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-18 14:43:38 -0600

    Create CharacterSearch.java

---
+++ 045_CharacterSearch/CharacterSearch.java
@@ -0,0 +1,12 @@
+public class CharacterSearch {
+    public static void main(String[] args){
+        //store string, also intitialize varibales to use later
+        String theString = "Sfndsiuf038.afsniyuf blebloooop, Il1 O00o00oo0oo0 dfanoe polar bear 98386.0488fs ][fdk30\\/=- florp zin0go l337?";
+        int letters = 0;
+        int numbers = 0;
+        int special_characters = 0;
+        
+
+    }
+    
+}
