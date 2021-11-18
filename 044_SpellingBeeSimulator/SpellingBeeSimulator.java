8b944a098e9a5d6d6412e3b5527e2d9b88fc18ca
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-18 14:27:22 -0600

    Create SpellingBeeSimulator.java

---
+++ 044_SpellingBeeSimulator/SpellingBeeSimulator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class SpellingBeeSimulator {
+    public static void main(String[] args){
+        //declare scanner, ask question,store vaiable
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your word");
+        String word = scan.nextLine();
+        System.out.print( word + ". " + word + " is spelled "); //opens up print statement
+        //start of for loop
+        for (int i = 0; i <= word.length() - 1; i++)// run until the word length
+        {
+            if (i == (word.length() - 1))//used to make sure last dash doesnt print
+            {
+                System.out.print(word.substring(i, i+1));// only runs when last letter
+            }
+            else
+            {
+                System.out.print(word.substring(i, i+1));//runs for all letters but last letter
+                System.out.print("-");
+            }
+        }
+        System.out.println("");//makes console look better
+
+    }
+    
+}
