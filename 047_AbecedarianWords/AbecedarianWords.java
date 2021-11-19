b40a96ab7ac9a7ff14a3fe04396e122ccad3c40e
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-19 11:23:57 -0600

    Create AbecedarianWords.java

---
+++ 047_AbecedarianWords/AbecedarianWords.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class AbecedarianWords {
+    public static void main(String[] args)
+    {
+        //boolean, for use later
+        boolean ab = true;
+        //decalre scanner ask question, store variable, change to lowercase
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input your word");
+        String wordBad = scan.nextLine();
+        String word = wordBad.toLowerCase();
+        scan.close();
+
+        for (int i = 0; i < (word.length() -1); i++)//for loop, runs unti word ends
+        {
+            String letter1 = word.substring(i, i+1);//used to compare letters
+            String letter2 = word.substring(i+1, i+2);
+            if (letter1.compareToIgnoreCase(letter2) <=0)// abcedarian checker
+            {
+                continue;
+            }
+            else{
+                ab = false;//breaks code and sets false if not abs
+                break;
+            }
+
+        }
+        //final checker for if abs or not
+        if (ab)
+        {
+            System.out.println("Your word is Abecedarian");
+        }
+        else{
+            System.out.println("Your word isnt Abecedarian");
+        }
+    }
+}
