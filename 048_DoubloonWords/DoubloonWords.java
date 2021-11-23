1d064217e3022fa0f290c9aa258f7d037c5674f7
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-23 11:12:31 -0600

    Update DoubloonWords.java

--- 048_DoubloonWords/DoubloonWords.java
+++ 048_DoubloonWords/DoubloonWords.java
@@ -1,10 +1,10 @@
-import java.uti.Scanner;
+import java.util.Scanner;
 public class DoubloonWords {
     public static void main(String args[]){
         //declare scanner, ask question, store variable
         Scanner scan = new Scanner(System.in);
         System.out.println("Input your word");
-        String word = scan.NextLine();
+        String word = scan.nextLine();
         scan.close();
 
     }
