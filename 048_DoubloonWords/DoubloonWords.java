6ca0328da793b90f38d56d7cc1e6fade2b9d2aaf
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-28 19:04:04 -0600

    Update DoubloonWords.java

--- 048_DoubloonWords/DoubloonWords.java
+++ 048_DoubloonWords/DoubloonWords.java
@@ -4,24 +4,51 @@ public class DoubloonWords {
         //declare scanner, ask question, store variable
         Scanner scan = new Scanner(System.in);
         System.out.println("Input your word");
-        String word = scan.nextLine();
+        String wordOg = scan.nextLine();
         scan.close();
+        //sets word to lower case
+        String word = wordOg.toLowerCase();
         //variable, for use in loop
-        int counter = 1;
+        int counter = 0;
+        boolean isDoubloon = true;
         //beginni gof for loop
         for (int i = 0; i < word.length(); i++)//runs until i is equal to word.length
         {
             // variables that store substrings, makes code cleaner
-            int a = word.substring(i, i+1)
+            counter = 0;
+            String a = word.substring(i, i+1);
             //start of embedded for loop
             for (int j = 0; j < word.length(); j++)
             {
-                if 
+                String b = word.substring(j, j+1);
+                if (b.equals(a))//tests if letters equal
                 {
+                    counter++;//adds to counter if same letter
                     
                 }
+                else
+                {
+                    continue;
+                }     
+            }
+            //if statemnts, checks counter and makes sure is exactly 2
+            if (counter == 2)
+            {
+                continue;
+            }
+            else{
+                isDoubloon = false;
+                break;
             }
         }
-
+        //final if statement checker
+        if (isDoubloon == true)
+        {
+            System.out.println("Your word is a doubloon word");
+        }
+        else
+        {
+            System.out.println("Your word isnt a doubloon word");
+        }
     }
 }
