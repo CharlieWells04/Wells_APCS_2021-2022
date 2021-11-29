192daead139d3c573a269145a0b8142f20494629
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-11-28 18:43:39 -0600

    Update DoubloonWords.java

--- 048_DoubloonWords/DoubloonWords.java
+++ 048_DoubloonWords/DoubloonWords.java
@@ -6,6 +6,22 @@ public class DoubloonWords {
         System.out.println("Input your word");
         String word = scan.nextLine();
         scan.close();
+        //variable, for use in loop
+        int counter = 1;
+        //beginni gof for loop
+        for (int i = 0; i < word.length(); i++)//runs until i is equal to word.length
+        {
+            // variables that store substrings, makes code cleaner
+            int a = word.substring(i, i+1)
+            //start of embedded for loop
+            for (int j = 0; j < word.length(); j++)
+            {
+                if 
+                {
+                    
+                }
+            }
+        }
 
     }
 }
