333432cefd0fbea867f95b1ff86972cd47350a5b
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-12-01 13:40:10 -0600

    Create NameGreeting.java

---
+++ 050_NameGreeting/NameGreeting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class NameGreeting {
+    //delare scanner, ask question, store variable
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+    
+    public static void main(String[] args)
+    {
+        greetName();
+    }
+    public static void greetName(String name)
+}
