c3ab503e13cc8049b9cce695b5a0af9ef5f7042c
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-12-01 13:34:07 -0600

    Create HelloWorldButAMethod.java

---
+++ 049_HelloWorldButAMethod/HelloWorldButAMethod.java
@@ -0,0 +1,10 @@
+public class HelloWorldButAMethod {
+    public static void main(String[] args)
+    {
+        helloWorld();
+    }
+    public static void helloWorld()
+    {
+        System.out.println("Hello World!");
+    }
+}
