98a90461b40d39546e0e05f866278a46c7684599
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-12-02 14:31:00 -0600

    Update NameGreeting.java

--- 050_NameGreeting/NameGreeting.java
+++ 050_NameGreeting/NameGreeting.java
@@ -3,15 +3,15 @@ public class NameGreeting {
     
     public static void main(String[] args)
     {
-        greetName();//runs greetName
-    }
-    public static void greetName()
-    {
     //delare scanner, ask question, store variable
     Scanner scan = new Scanner(System.in);
     System.out.println("What is your name?");
     String nameOfPerson = scan.nextLine();
     scan.close();
-    System.out.println("Hello " + nameOfPerson);//prints out greeting
+    greetName(nameOfPerson);//runs greetName
+    }
+    public static void greetName(String name)
+    {
+    System.out.println("Hello " + name);//prints out greeting
     }
 }
