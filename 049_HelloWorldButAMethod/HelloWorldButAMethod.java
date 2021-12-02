cfe80759a3383968fd5cb0f0bd0e0e1c797efd39
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-12-02 13:44:42 -0600

    Update HelloWorldButAMethod.java

--- 049_HelloWorldButAMethod/HelloWorldButAMethod.java
+++ 049_HelloWorldButAMethod/HelloWorldButAMethod.java
@@ -1,9 +1,9 @@
 public class HelloWorldButAMethod {
-    public static void main(String[] args)
+    public static void main(String[] args)//the main method, calls the other method
     {
         helloWorld();
     }
-    public static void helloWorld()
+    public static void helloWorld()//called and prins hello world
     {
         System.out.println("Hello World!");
     }
