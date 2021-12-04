a51d4f87006c914870644b6edae74d98892ddf22
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-12-03 22:28:20 -0600

    Update ZodiacCalculator.java

--- 051_ZodiacCalculatorOne/ZodiacCalculator.java
+++ 051_ZodiacCalculatorOne/ZodiacCalculator.java
@@ -9,15 +9,55 @@ public class ZodiacCalculator {
         scan.close();
         zodiac(yearOfBirth);
     }
-    public static void zodiac(int year)
+    public static void zodiac(int year)//function to figr=ure out zodiac
     {
         if (year % 12 == 0)
         {
-            System.out.println("You were born in the year of the monkey");
+            System.out.println("You were born in the year of the monkey");//use modulo to find out year, 12 totoal year it rotates onl, repeat 12 times
         }
         else if (year % 12 == 1)
         {
             System.out.println("You were born in the year of the Rooster");
+        }
+        else if (year % 12 == 2)
+        {
+            System.out.println("You were born in the year of the Dog");
+        }
+        else if (year % 12 == 3)
+        {
+            System.out.println("You were born in the year of the Pig");
+        }
+        else if (year % 12 == 4)
+        {
+            System.out.println("You were born in the year of the Rat");
+        }
+        else if (year % 12 == 5)
+        {
+            System.out.println("You were born in the year of the Ox");
+        }
+        else if (year % 12 == 6)
+        {
+            System.out.println("You were born in the year of the Tiger");
+        }
+        else if (year % 12 == 7)
+        {
+            System.out.println("You were born in the year of the Rabbit");
+        }
+        else if (year % 12 == 8)
+        {
+            System.out.println("You were born in the year of the Dragon");
+        }
+        else if (year % 12 == 9)
+        {
+            System.out.println("You were born in the year of the Snake");
+        }
+        else if (year % 12 == 10)
+        {
+            System.out.println("You were born in the year of the Horse");
+        }
+        else if (year % 12 == 11)
+        {
+            System.out.println("You were born in the year of the Sheep");
         }
     }
     
