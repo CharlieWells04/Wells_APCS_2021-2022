cff0cd10bdc9c11e7a4a4401d8427bc3a900a856
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-12-06 13:35:31 -0600

    Create Closet.java

---
+++ 053_FindAShirtUno/Closet.java
@@ -0,0 +1,11 @@
+public class Closet {
+    public static void main(String[] args)
+    {
+        //call in shirt method
+        Shirt s = new Shirt();
+        s.selfDescription();
+        s.fold();
+        s.cleanStatus();
+    }
+    
+}
