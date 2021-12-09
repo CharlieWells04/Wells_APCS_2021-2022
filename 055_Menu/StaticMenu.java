7fcc4cf84656c81a1dde49533647818dd8809fef
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2021-12-09 12:35:07 -0600

    Update StaticMenu.java

--- 055_Menu/StaticMenu.java
+++ 055_Menu/StaticMenu.java
@@ -1,3 +1,13 @@
-public class StaticMenu {
-    
+public class StaticMenu {//accciedentally worte wrong code in wrong class, class names should be flipped 
+    //making static class variables
+    public static String itemName;
+    public static String itemDescription;
+    public static void main(String[] args)
+    {
+        //empty
+    }
+    public static void printItem()
+    {
+        System.out.println(itemName + ": " + itemDescription);
+    }
 }
