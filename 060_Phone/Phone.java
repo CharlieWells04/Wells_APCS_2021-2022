b4c7b6df78c8a28f704d9e620315b4ff897c44a1
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-01-05 13:39:42 -0600

    Update Phone.java

--- 060_Phone/Phone.java
+++ 060_Phone/Phone.java
@@ -1,6 +1,28 @@
 public class Phone {
+    //6 class variables, as requested
     int currentCharge;
     String connectionType;
+    int screenSize;
+    String color;
+    int screenSize;
+    int price;
+
+    //main, is emppty
+    public static void main(String[] args)
+    {
+        //empty
+    }
+    //base constructor
+    public static void Phone(currentCharge, connectionType, screenSize, color, screenSize, price)
+    {
+        this.currentCharge = currentCharge;
+        this.connectionType = connectionType;
+        this.screenSize = screenSize;
+        this.color = color;
+        this.screenSize = screenSize;
+        this.price = screenSize;
+    }
+
 
 
     
