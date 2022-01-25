82b20b5f9b3f52ee37a4e6769f264518c4ea52d1
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-01-25 14:27:15 -0600

    Create ClassLister.java

---
+++ 067_ClassLister/ClassLister.java
@@ -0,0 +1,13 @@
+public class ClassLister {
+    public static void main(String[] args)
+    {
+        //creating array
+        String[] classes = {"Calculus", "Gym", "Band", "Physics", "Spectrum", "APCS"};
+        //creating for loop
+        for(int i = 0; i < classes.length; i++)
+        {
+            System.out.println(classes[i]);
+        }
+    }
+    
+}
