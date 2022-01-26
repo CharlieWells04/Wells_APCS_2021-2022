acf65102e02eb63d2943f850ab0afb86f57b421e
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-01-26 12:35:08 -0600

    fafddas

--- 068_DeckOfCards/Deck.java
+++ 068_DeckOfCards/Deck.java
@@ -1,23 +1,76 @@
 public class Deck {
     //creating all the cards
+    //hearts
     Card h1 = new Card("Hearts" , 1);
     Card h2 = new Card("Hearts" , 2);
     Card h3 = new Card("Hearts" , 3);
-    Card h4 = new Card("Hearts" , 1);
-    Card h5 = new Card("Hearts" , 1);
-    Card h6 = new Card("Hearts" , 1);
-    Card h7 = new Card("Hearts" , 1);
-    Card h8 = new Card("Hearts" , 1);
-    Card h9 = new Card("Hearts" , 1);
-    Card h10 = new Card("Hearts" , 1);
-    Card h11 = new Card("Hearts" , 1);
-    Card h12 = new Card("Hearts" , 1);
-    Card h13 = new Card("Hearts" , 1);
+    Card h4 = new Card("Hearts" , 4);
+    Card h5 = new Card("Hearts" , 5);
+    Card h6 = new Card("Hearts" , 6);
+    Card h7 = new Card("Hearts" , 7);
+    Card h8 = new Card("Hearts" , 8);
+    Card h9 = new Card("Hearts" , 9);
+    Card h10 = new Card("Hearts" , 10);
+    Card h11 = new Card("Hearts" , 11);
+    Card h12 = new Card("Hearts" , 12);
+    Card h13 = new Card("Hearts" , 13);
+    //spades
+    Card s1 = new Card("Spades" , 1);
+    Card s2 = new Card("Spades" , 2);
+    Card s3 = new Card("Spades" , 3);
+    Card s4 = new Card("Spades" , 4);
+    Card s5 = new Card("Spades" , 5);
+    Card s6 = new Card("Spades" , 6);
+    Card s7 = new Card("Spades" , 7);
+    Card s8 = new Card("Spades" , 8);
+    Card s9 = new Card("Spades" , 9);
+    Card s10 = new Card("Spades" , 10);
+    Card s11 = new Card("Spades" , 11);
+    Card s12 = new Card("Spades" , 12);
+    Card s13 = new Card("Spades" , 13);
+
+    //diamonds
+    Card d1 = new Card("Diamonds" , 1);
+    Card d2 = new Card("Diamonds" , 2);
+    Card d3 = new Card("Diamonds" , 3);
+    Card d4 = new Card("Diamonds" , 4);
+    Card d5 = new Card("Diamonds" , 5);
+    Card d6 = new Card("Diamonds" , 6);
+    Card d7 = new Card("Diamonds" , 7);
+    Card d8 = new Card("Diamonds" , 8);
+    Card d9 = new Card("Diamonds" , 9);
+    Card d10 = new Card("Diamonds" , 10);
+    Card d11 = new Card("Diamonds" , 11);
+    Card d12 = new Card("Diamonds" , 12);
+    Card d13 = new Card("Diamonds" , 13);
+    //clubs
+    Card c1 = new Card("Clubs" , 1);
+    Card c2 = new Card("Clubs" , 2);
+    Card c3 = new Card("Clubs" , 3);
+    Card c4 = new Card("Clubs" , 4);
+    Card c5 = new Card("Clubs" , 5);
+    Card c6 = new Card("Clubs" , 6);
+    Card c7 = new Card("Clubs" , 7);
+    Card c8 = new Card("Clubs" , 8);
+    Card c9 = new Card("Clubs" , 9);
+    Card c10 = new Card("Clubs" , 10);
+    Card c11 = new Card("Clubs" , 11);
+    Card c12 = new Card("Clubs" , 12);
+    Card c13 = new Card("Clubs" , 13);
     //creating array
-    deck[] = {};
-    
+    private Object[] deck = {h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, s1, s2 ,s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13};
+    //creating constructor
+    public Deck()
+    {
+        //empty
+    }
+    //making a getdeck method
+    public Object[] getDeck()
+    {
+        return deck;
+    }
+    public Object getSuit
     //empty main method
 
-    public static void main(String[] args){}
-    
+    public static void main(String[] args){} 
 }
--- 068_DeckOfCards/Table.java
+++ 068_DeckOfCards/Table.java
@@ -1,4 +1,16 @@
 public class Table {
+    public static void main(String[] args)
+    {
+        Deck deck = new Deck();
+        //for loop to print out all the cards
+        for(int i = 0; i < deck.getDeck().length; i++)
+        {
+            if (deck.getDeck() == 1)
+            {
+                System.out.println("1");
+            }
+        }
+    }
 
     
 }
