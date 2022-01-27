12d9173f850bf4ed904ea15e86251f7e43ab0a1e
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-01-27 11:24:33 -0600

    asdfkadsopifpuiadjfiadslhfp9uasdfiahdsnf

--- 068_DeckOfCards/Table.java
+++ 068_DeckOfCards/Table.java
@@ -1,14 +1,35 @@
 public class Table {
     public static void main(String[] args)
     {
-        Deck deck = new Deck();
+        Deck d1 = new Deck();
+        //maikng a new card array
+        Card[] cards = d1.getDeck();
+
         //for loop to print out all the cards
-        for(int i = 0; i < deck.getDeck().length; i++)
+        for(int i = 0; i < 52; i++)
         {
-            if (deck.getDeck() == 1)
+            //if statement, prints out correct value
+            if(cards[i].getValue() == 1)
             {
-                System.out.println("1");
+                System.out.print("Ace of ");
             }
+            else if(cards[i].getValue() == 11)
+            {
+                System.out.print("Jack of ");
+            }
+            else if(cards[i].getValue() == 12)
+            {
+                System.out.print("Ace of ");
+            }
+            else if(cards[i].getValue() == 13)
+            {
+                System.out.print("King of ");
+            }
+            else
+            {
+                System.out.print(cards[i].getValue() + " of ");
+            }
+            System.out.println(cards[i].getSuit());//statement that prints out suit
         }
     }
 
