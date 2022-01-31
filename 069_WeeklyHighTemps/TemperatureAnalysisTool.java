50fdf043cacf71ff9663297ade7c0ca49db7d43c
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-01-31 12:27:27 -0600

    fniozgdfipojdgopisng

---
+++ 069_WeeklyHighTemps/TemperatureAnalysisTool.java
@@ -0,0 +1,9 @@
+public class TemperatureAnalysisTool {
+    //arrays for usage
+    String[] date = {"2/1/22", "2/2/22", "2/3/22", "2/4/22", "2/5/22", "2/6/22", "2/7/22"};
+    int[] highs = {34, 9, 6, 16, 26, 22, 25};
+    int[] lows = {-2, -10, -7, -2, 18, 5,17};
+    String[] pwp = {"Cloudy. Morning high of 34F with temps falling to near 15. Winds WNW at 15 to 25 mph. Higher wind gusts possible.","Partly cloudy. Low -2F. Winds NW at 10 to 20 mph.", "Mainly cloudy. High 9F. Winds NNW at 10 to 15 mph.", "Sunny skies. High 6F. Winds N at 5 to 10 mph.", "Intervals of clouds and sunshine in the morning with more clouds for later in the day. High 26F. Winds S at 10 to 15 mph.", "Intervals of clouds and sunshine. High 22F. Winds NW at 10 to 15 mph.", "Partly cloudy skies. High around 25F. Winds SSW at 5 to 10 mph."};
+    //main method
+    public static void main(String[] args){};
+}
