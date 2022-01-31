4676be81ef5f9158f271bba3db02f364876c7c0b
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-01-31 14:32:02 -0600

    ert

--- 069_WeeklyHighTemps/TemperatureAnalysisTool.java
+++ 069_WeeklyHighTemps/TemperatureAnalysisTool.java
@@ -1,9 +1,21 @@
 public class TemperatureAnalysisTool {
-    //arrays for usage
-    String[] date = {"2/1/22", "2/2/22", "2/3/22", "2/4/22", "2/5/22", "2/6/22", "2/7/22"};
-    int[] highs = {34, 9, 6, 16, 26, 22, 25};
-    int[] lows = {-2, -10, -7, -2, 18, 5,17};
-    String[] pwp = {"Cloudy. Morning high of 34F with temps falling to near 15. Winds WNW at 15 to 25 mph. Higher wind gusts possible.","Partly cloudy. Low -2F. Winds NW at 10 to 20 mph.", "Mainly cloudy. High 9F. Winds NNW at 10 to 15 mph.", "Sunny skies. High 6F. Winds N at 5 to 10 mph.", "Intervals of clouds and sunshine in the morning with more clouds for later in the day. High 26F. Winds S at 10 to 15 mph.", "Intervals of clouds and sunshine. High 22F. Winds NW at 10 to 15 mph.", "Partly cloudy skies. High around 25F. Winds SSW at 5 to 10 mph."};
-    //main method
-    public static void main(String[] args){};
+    //adding in weekly high temps
+    WeeklyHighTemps w1 = new WeeklyHighTemps();
+    //making the new arrays for use
+    String[] dates = w1.getDate();
+    int[] highs = w1.getHighs();
+    int[] lows = w1.getLows();
+    String[] patterns = w1.getPatterns();
+    public int temperatureAverage()
+    {
+        int count = 0;
+        for(int i = 0; i < 7; i++) 
+        {
+            count = count + highs[i] + lows[i];
+        }
+        return count/14;
+    }
+    
+    public static void main(String[] args){}
+    }
 }
--- 069_WeeklyHighTemps/WeeklyHighTemps.java
+++ 069_WeeklyHighTemps/WeeklyHighTemps.java
@@ -1,3 +1,25 @@
 public class WeeklyHighTemps {
-    
+    //arrays for usage
+    private String[] date = {"2/1/22", "2/2/22", "2/3/22", "2/4/22", "2/5/22", "2/6/22", "2/7/22"};
+    private int[] highs = {34, 9, 6, 16, 26, 22, 25};
+    private int[] lows = {-2, -10, -7, -2, 18, 5,17};
+    private String[] patterns = {"cloudy", "mostly cloudy", "partly cloudy", "mainly cloudy", "partly cloudy"};
+    //making getters
+    public String[] getDate()
+    {
+        return date;
+    }
+    public String[] getPatterns()
+    {
+        return patterns;
+    }
+    public int[] getHighs()
+    {
+        return highs;
+    }
+    public int[] getLows()
+    {
+        return lows;
+    }
+    public static void main(String[] args){}
 }
