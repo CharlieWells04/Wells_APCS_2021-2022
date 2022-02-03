785ae3fcab0b4bf54a9620843daa3541e6b637af
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-02-03 14:40:31 -0600

    feSDFsf

--- 069_WeeklyHighTemps/TemperatureAnalysisTool.java
+++ 069_WeeklyHighTemps/TemperatureAnalysisTool.java
@@ -1,3 +1,5 @@
+import javax.lang.model.util.ElementScanner14;
+
 public class TemperatureAnalysisTool {
     //adding in weekly high temps
     WeeklyHighTemps w1 = new WeeklyHighTemps();
@@ -15,7 +17,55 @@ public class TemperatureAnalysisTool {
         }
         return count/14;
     }
-    
-    public static void main(String[] args){}
+    public String patternAverage()
+    {
+        int cloudys = 0;///creating vribales as trackers for the amount of times a pattern occurs
+        int partlyCloudys = 0;
+        int mostlyCloudys = 0;
+        for(int i = 0; i < patterns.length; i++)//iterates thru and adds them up
+        {
+            if (patterns[i].equals("cloudy"))
+            {
+            cloudys++;
+            }
+            else if (patterns[i].equals("partly cloudy"))
+            {
+            partlyCloudys++;
+            }
+            else 
+            {
+            mostlyCloudys++;
+            }
+        }
+        if(cloudys > mostlyCloudys || cloudys > partlyCloudys)//making an if statement to return which one had the most
+        {
+            return "Cloudy";
+        }
+        else if(partlyCloudys > mostlyCloudys || cloudys < partlyCloudys)
+        {
+            return "partly cloudy";
+        }
+        else if(partlyCloudys < mostlyCloudys || cloudys < mostlyCloudys)
+        {
+            return "mostly cloudy";
+        }
+        else
+        {
+            return "a tie ";
+        }
+
+    }
+    public void printWeatherForeCast()
+    {
+        int high = 33;//addign variables
+        int low = 12;
+        int average = (high+low) / 2;
+        String pattern = "Rainy";
+        //printing
+        System.out.println("The high is " + high + ", the low is " + low + "the average is " + average + "and is is " + pattern );
+
+    }  
+    public static void main(String[] args){
     }
 }
+
--- 069_WeeklyHighTemps/WeeklyHighTemps.java
+++ 069_WeeklyHighTemps/WeeklyHighTemps.java
@@ -3,7 +3,7 @@ public class WeeklyHighTemps {
     private String[] date = {"2/1/22", "2/2/22", "2/3/22", "2/4/22", "2/5/22", "2/6/22", "2/7/22"};
     private int[] highs = {34, 9, 6, 16, 26, 22, 25};
     private int[] lows = {-2, -10, -7, -2, 18, 5,17};
-    private String[] patterns = {"cloudy", "mostly cloudy", "partly cloudy", "mainly cloudy", "partly cloudy"};
+    private String[] patterns = {"cloudy", "mostly cloudy", "partly cloudy", "cloudy", "partly cloudy"};
     //making getters
     public String[] getDate()
     {
