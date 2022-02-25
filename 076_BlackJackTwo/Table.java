d8da63bb89090f06017ab048febfda4f124ae92d
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-02-25 14:00:21 -0600

    dsa

--- 076_BlackJackTwo/Table.java
+++ 076_BlackJackTwo/Table.java
@@ -1,14 +1,36 @@
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.Scanner;
 public class Table
 {
     public static void main(String[] args)
     {
-        Deck deck = new Deck();
-        Card[] cardDeck = deck.getDeck();
-        Table table = new Table();
-        BlackJack blackjack = new BlackJack(cardDeck);
-        blackjack.runHand();
+        while(true)//loop allows iteration and playing multiple times
+        {
+            Deck deck = new Deck();
+            Card[] cardDeck = deck.getDeck();
+            Table table = new Table();
+            BlackJack blackjack = new BlackJack(cardDeck);
+            blackjack.runHand();//method to run the hadn
+            //adding in scanner
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Would you like to play again (input \"y\" or \"n\")");
+            String YorN = scan2.nextLine();
+            //if statement, checks if user wants to play again
+            if(YorN.equals("n"))
+            {
+                System.out.println("Game ended");
+                break;
+            }
+            else if (YorN.equals("y"))
+            {
+                System.out.println("Playing again");
+            }
+            else
+            {
+                System.out.println("Input not detected, playing again");
+            }
+            
+        }
     }
 }
