a4a90281ccb5152cbb9056e3a4995be6b425d313
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-02-28 14:42:31 -0600

    asdf

--- 077_LinearVBinary/BinarySearch.java
+++ 077_LinearVBinary/BinarySearch.java
@@ -1,3 +1,40 @@
 public class BinarySearch {
-    
+    public static void main(String[] args){
+    //creating the stuff beofre the sorting happens
+    OneThousandSortedRandomNumbers x = new OneThousandSortedRandomNumbers();
+    int[] arrayToSearch = x.getSortedNumbers();
+    BinarySearch b = new BinarySearch();
+    b.BinarySearchFind(arrayToSearch, 8388);
+    b.BinarySearchFind(arrayToSearch, 9807);
+    b.BinarySearchFind(arrayToSearch, 4011);
+    b.BinarySearchFind(arrayToSearch, 5423);
+    }
+    //making the method for sorting linearly
+    public int BinarySearchFind(int[] arr, int numToFind)
+    {
+        int first = 0;//creates a right side at 0
+        int last = arr.length;//creates a left side at the array length
+        int mid = (first + last)/2; //createsa  middle inde int the middle
+        int count = 0; //used for conting iterations
+        while( first <= last ){  //loops thru until found or not found
+        count++;//adds to count every iteration
+           if ( arr[mid] < numToFind ){  //checks if midpoint is less than the number
+             first = mid + 1;//sets the beginning to the mid point +1     
+           }else if ( arr[mid] == numToFind ){  //checls of the target and number are equa;
+             System.out.println("Number is found at index: " + mid + " It took " + count + " iterations to find the number");
+             return mid;  //returns the index
+           }else{  
+              last = mid - 1;  //sets the end to the mid point = 1    
+           }  
+           mid = (first + last)/2;  
+           if ( first > last ){  //checks if the first has passed the last, if this happens the umber doesnt exists in the array you are searcing
+            System.out.println("The Number " + numToFind + " was not found");
+            return -1;//prints out -1 if not found
+            }  
+        }
+        return -1;//this should never trigger
+    } 
 }
+
+
+
--- 077_LinearVBinary/LinearSearch.java
+++ 077_LinearVBinary/LinearSearch.java
@@ -1,3 +1,31 @@
 public class LinearSearch {
-    
+    public static void main(String[] args){
+    //creating the stuff beofre the sorting happens
+    OneThousandSortedRandomNumbers x = new OneThousandSortedRandomNumbers();
+    int[] arrayToSearch = x.getSortedNumbers();
+    LinearSearch b = new LinearSearch();
+    b.LinearSearchFind(arrayToSearch, 8388);
+    b.LinearSearchFind(arrayToSearch, 9807);
+    b.LinearSearchFind(arrayToSearch, 4011);
+    b.LinearSearchFind(arrayToSearch, 5423);
+    }
+    //making the method for sorting linearly
+    public int LinearSearchFind(int[] arr, int numToFind)
+    {
+        int count = 0;
+        for(int i = 0; i < arr.length; i++)//runs thru whole array
+        {
+            if (arr[i] == numToFind)//if num is equal. return the index
+            {
+                count++;
+                System.out.println("The first insatnce of " + numToFind + " is at index " + i + ", it took " + count + " iterations to find the index");
+                return i;
+            }
+            else{
+                count++;
+            }
+        }
+        System.out.println("The Number " + numToFind + " was not found");
+        return -1;//prints out -1 if not found
+    }
 }
