d875c53a816d23466414703d31eccc10b79bc4b6
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-03-21 12:59:40 -0500

    bfncjnvzb

---
+++ 078_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class SelectionSortSomeIntegers {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many numbers would you like me to generate (1-1000)");
+              
+    }
+}
