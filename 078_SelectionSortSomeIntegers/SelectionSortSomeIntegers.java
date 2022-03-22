c16f010fab0d7834baee676fae1d85bd482c920d
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-03-22 11:45:34 -0500

    afds

--- 078_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
+++ 078_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
@@ -2,8 +2,16 @@ import java.util.*;
 public class SelectionSortSomeIntegers {
     public static void main(String[] args)
     {
+        //making scanner, asking question, storing answer
         Scanner scan = new Scanner(System.in);
         System.out.println("How many numbers would you like me to generate (1-1000)");
-              
+        int numOfInts = scan.nextInt();  
+        //making the arraylis to host the random ints
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < numOfInts; i++)//looping thru and generating ints
+        {
+            nums.add((int)(Math.random() * 1000 + 1));
+        }
+        //doing the slection sort thing        
     }
 }
