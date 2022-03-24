7196110aae593ee46bd4935ae5e58b74943e78a5
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-03-24 14:38:32 -0500

    Update SelectionSortSomeIntegers.java

--- 078_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
+++ 078_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
@@ -12,6 +12,21 @@ public class SelectionSortSomeIntegers {
         {
             nums.add((int)(Math.random() * 1000 + 1));
         }
-        //doing the slection sort thing        
+        //doing the slection sort thing
+        for (int i = 0; i < nums.size() - 1; i++) {
+            int pos = i;
+            // find position of smallest num between (i + 1)th element and last element
+            for (int j = i + 1; j <= nums.size(); j++) {
+                if (nums.get(i) < nums.get(pos))
+                {
+                pos = j;
+                }
+                // Swap min (smallest num) to current position on array
+                int min = nums.get(pos);
+                nums.get(pos) = nums.get(i);
+                nums.get(i) = min;
+            }    
+        System.out.println(nums);     
     }
+}
 }
