b98a50c3680b9f3e99a609e5bb13b1c6d298ff1b
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-03-25 13:38:12 -0500

    eF

---
+++ 079_ScabbleCalculator/ScrabbleTile.java
@@ -0,0 +1,10 @@
+public class ScrabbleTile {
+    public String letter;
+    public int points;
+    public ScrabbleTile(String letter, int points)
+    {
+        this.letter = letter;
+        this.points = points;
+    }
+    
+}
