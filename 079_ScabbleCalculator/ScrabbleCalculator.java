d2d2159fbf13d28e7e2a276902d856c656029fa4
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-04-04 22:53:01 -0500

    sdfg

--- 079_ScabbleCalculator/ScrabbleCalculator.java
+++ 079_ScabbleCalculator/ScrabbleCalculator.java
@@ -3,42 +3,65 @@ public class ScrabbleCalculator {
     public ArrayList<ScrabbleTile> userWordTiles = new ArrayList<>();
     public void printUserTiles()
     {
-        System.out.println(userWordTiles);
+        //prints out the users letter
+        for(int i = 0; i < userWordTiles.size(); i++)
+        {
+            String temp2 = userWordTiles.get(i).getLetter();
+            System.out.print(temp2 + " ");
+        }
+        System.out.println("");
     }
-    public void CheckWord(String[] word)
+    public boolean CheckWord(String[] word)
     {
         //first set of for for loops, checks and sees if word works
         //first iteration, loops thru the word 
+        ArrayList<ScrabbleTile> temp = new ArrayList<>();
+        temp = userWordTiles;//temp is created to check and remove without actually removing them
+        boolean failedLetter = true;//used later to break loop if letter fails
         for(int i = 0; i < word.length; i++)
         {
             //second iteration, loops through the userword tiles, checks if they match
-            ArrayList<ScrabbleTile> temp = new ArrayList<>();
-            temp = userWordTiles;//temp is created to check and remove without actually removing them
-            for(int j = 0; i < temp.size(); i++)
+            for(int j = 0; j < temp.size(); j++)
             {
-                if(word[i].equals(temp.get(j)))
+                failedLetter = true;//sets to true for each lettr
+                if(word[i].equals(temp.get(j).getLetter()))
                 {
                     temp.remove(j);
+                    failedLetter = false;
+                    break;//breaks the loop because succesful letter has been found
                 }
-                else{
-                    System.out.println("Word doesnt work, try again");
-                    break;
-                }
+            }
+            if(failedLetter == true)
+            {
+                System.out.println("Word doesnt work, try again");//kills the loop and prints off negative statemnt if word doesnt work
+                return false;
             }
         }
         System.out.println("The word works");
-        //second set of for loops, actually removes the letters from the users arraylist
+        //second set of for loops, actually removes the letters from the users arraylist, much simpler due to knowing that word workss
         for(int i = 0; i < word.length; i++)
         {
-            for(int j = 0; i < userWordTiles.size(); i++)
+            //second iteration, loops through the userword tiles, checks if they match
+            for(int j = 0; j < userWordTiles.size(); j++)
             {
-                if(word[i].equals(userWordTiles.get(j)))
+                if(word[i].equals(userWordTiles.get(j).getLetter()))
                 {
                     userWordTiles.remove(j);
+                    break;//breaks the loop because succesful letter has been found
                 }
             }
         }
+        return true;
     }
+    public void fillUserTiles(ScrabbleTile[] bagToUse)//fills up users tiles, back up to 7
+    {
+        while(userWordTiles.size() < 7 && bagToUse.length > 0)
+        {
+            int rando = (int)(Math.random() * bagToUse.length);
+            userWordTiles.add(bagToUse[rando]);
+        }
+    }
+
     public static void main(String[] args){}
     
 }
--- 079_ScabbleCalculator/Table.java
+++ 079_ScabbleCalculator/Table.java
@@ -1,3 +1,36 @@
+import java.io.*;
+import java.util.*;
 public class Table {
-    
+    public static void main(String[] args)//making all the stuff in main so that the program actually runs
+    {
+        Table table = new Table();
+        ScrabbleBag sb = new ScrabbleBag();
+        table.RunGame(sb.getBag());
+    }
+    public void RunGame(ScrabbleTile[] originalBag)
+    {
+        ScrabbleCalculator sc = new ScrabbleCalculator();//creating the scrabble calculator and setting up the userwordtiles
+        sc.fillUserTiles(originalBag);
+        //will add loop herer later
+            int totalPoints = 0;
+            System.out.println("Here are your letters");//showing user their letters for this hand
+            sc.printUserTiles();
+            //making scanner to scan in word
+            Scanner scan = new Scanner(System.in);
+            System.out.println("What word would you like to spell?");
+            String userWordButString = scan.nextLine();
+            scan.close();
+            //i copied this part from google, essentially what it does is turns the word into an array that i can then sort thru and check
+            char[] userWordButChar = userWordButString.toCharArray();
+            //this part then goes thru and sets all the chars to strings in a new array so that i can compare them
+            String[] userWord = new String[userWordButChar.length];
+            for(int i = 0; i < userWordButChar.length; i++)
+            {
+                userWord[i] = ("" + userWordButChar[i]);//this is a kind of scuffed workaraound, but it turns the char into a string
+            }
+            if(sc.CheckWord(userWord) == true)
+            {
+                //all is left to do is to make the methgod that calculates and pritns points
+            }
+    }
 }
